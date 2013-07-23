fb7689b6711023db205e295e08a2ef3a97e5e677
HoonBeum <hbtd@naver.com>
2013-07-23 10:19:11 +0900

    first commit

--- 2주차/If-Make/src/BMW.java
+++
@@ -1,6 +0,0 @@
-
-public class BMW {
-    public void BMWCar(){
-        System.out.println("BMW매장에 오신것을 환영합니다.");
-    }
-}
--- 4주차/spring/src/spring/BMW.java
+++
@@ -1,7 +0,0 @@
-package spring;
-
-public class BMW extends Cars implements Select{
-    public void Print(){
-        System.out.println("BMW매장에 오신것을 환영합니다.");
-    }
-}
